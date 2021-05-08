e81645def2d4fc1726718ba6330a6391fb755442
Saufaer <ogretoleg@yandex.ru>
2021-05-08 19:22:22 +0300

    new searcher for E

--- Points_Outflow.h
+++ Points_Outflow.h
@@ -4,7 +4,7 @@
 #include "Rule_6_7_8_9.h"
 double M5Max(double alpha, double gamma2)
 {
-    double M5max = sqrt((1 + (1 - alpha)*gamma2) - sqrt(pow((1 + (1 - alpha)*gamma2), 2) - alpha * alpha));
+    double M5max = sqrt((1 + (1 - alpha)*gamma2) - sqrt(powf((1 + (1 - alpha)*gamma2), 2) - powf(alpha,2)));
     return M5max;
 }
 Point B(START start)
@@ -40,31 +40,61 @@ Point E(START start)
     Point e{ NULL };
 
     double M5max = M5Max(ALPHA1(start.A1, start.A2), start.gamma2);
-    double p = start.p2;
-    double u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
+    e = Point{ 0,0 };
+    //double p = start.p2;
+    //double u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
 
-    {
-        if (u < M5max * start.c2) {//go up from (p2,u2)
-            while (true)
-            {
-                if (u <= M5max * start.c2) { p -= 0.1; }
-                else { break; }
-                u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
-            }
-        }
-        else//go down from (p2,u2)
+        //if (u < M5max * start.c2) {//go up from (p2,u2)
+        //    while (true)
+        //    {
+        //        if (fabs(u - M5max * start.c2) <= 1) { break; } 
+        //         p -= 0.01; 
+        //         if (u <= 0) { break; }
+        //        u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
+        //    }
+        //}
+        //else//go down from (p2,u2)
         {
+
             double p = start.p2;
             double u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
-            while (true)
+
+           
+            if (u < M5max * start.c2)//go up from (p2,u2)
             {
-                if (u > M5max * start.c2) { p += 0.1; }
-                else { break; }
+                while (true) // go up from p2u2
+                {
+                    if (fabs(u - M5max * start.c2) <= 10) {
+                        break;
+                        e = Point{ p,u };
+                        return e;
+                    }
+                    p -= 0.1;
+                    if (p <= 0) { break; }
+                    if (u <= 0) { break; }
+                    u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
+                }
+            }
+            else {
+                cout << "\n\n Search E down \n\n M5max * start.c2 = " << M5max * start.c2 << "\n\n u = " << u << "";
+                p = start.p2;
                 u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
+                while (true) // go down from p2u2
+                {
+                    //cout << "\n\n u = " << u << "";
+                    if (fabs(u - M5max * start.c2) <= 10) {
+                        break;
+                        e = Point{ p,u };
+                        return e;
+                    }
+                    p += 0.1;
+                    if (u <= 0) { break; }
+                    if (p <= 0) { break; }
+                    u = L2(p, start.u2, start.p2, start.ro2, start.gamma2, start.c2);
+                }
             }
         }
-    }
-    e = Point{ p,u };
+
     return e;
 }
 
